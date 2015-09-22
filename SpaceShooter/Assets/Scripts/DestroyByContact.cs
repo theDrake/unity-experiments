f357c076ddd7666205aa5c92e1619eedf5ca2609
theDrake <dcdrake42@gmail.com>
2015-09-21 19:13:32 -0600

    Add asteroids

---
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+  public GameObject explosion, playerExplosion;
+
+  private Transform thisTransform, otherTransform;
+
+  void Start()
+  {
+    thisTransform = GetComponent<Transform>();
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Boundary")
+      return;
+    Destroy(gameObject);
+    Destroy(other.gameObject);
+    Instantiate(explosion, thisTransform.position, thisTransform.rotation);
+    if (other.tag == "Player")
+    {
+      otherTransform = other.GetComponent<Transform>();
+      Instantiate(playerExplosion,
+                  otherTransform.position,
+                  otherTransform.rotation);
+    }
+  }
+}
--- SpaceShooter/Assets/Scripts/PlayerController.cs
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,12 @@ public class PlayerController : MonoBehaviour
   public Transform shotSpawn;
 
   private float lastShotTime;
+  private Rigidbody rb;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
 
   void Update()
   {
@@ -31,18 +37,18 @@ public class PlayerController : MonoBehaviour
     float moveVertical = Input.GetAxis("Vertical");
 
     Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-    GetComponent<Rigidbody>().velocity = movement * speed;
-    GetComponent<Rigidbody>().position = new Vector3
+    rb.velocity = movement * speed;
+    rb.position = new Vector3
       (
-        Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+        Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
         0.0f,
-        Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
       );
-    GetComponent<Rigidbody>().rotation = Quaternion.Euler
+    rb.rotation = Quaternion.Euler
       (
         0.0f,
         0.0f,
-        GetComponent<Rigidbody>().velocity.x * -tilt
+        rb.velocity.x * tilt
       );
   }
 }
