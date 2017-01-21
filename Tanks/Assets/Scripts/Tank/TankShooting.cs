9073995d3fc68912d6553a6a05dd82437fb3f6c3
theDrake <theDrake@users.noreply.github.com>
2017-01-20 22:12:40 -0800

    Add shooting functionality

--- Tanks/Assets/Scripts/Tank/TankShooting.cs
+++ Tanks/Assets/Scripts/Tank/TankShooting.cs
@@ -13,31 +13,47 @@ public class TankShooting : MonoBehaviour {
   public float m_MaxLaunchForce = 30f;
   public float m_MaxChargeTime = 0.75f;
 
-  /*
   private string m_FireButton;
   private float m_CurrentLaunchForce;
   private float m_ChargeSpeed;
   private bool m_Fired;
 
-
   private void OnEnable() {
-      m_CurrentLaunchForce = m_MinLaunchForce;
-      m_AimSlider.value = m_MinLaunchForce;
+    m_CurrentLaunchForce = m_MinLaunchForce;
+    m_AimSlider.value = m_MinLaunchForce;
   }
 
   private void Start() {
-      m_FireButton = "Fire" + m_PlayerNumber;
-
-      m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    m_FireButton = "Fire" + m_PlayerNumber;
+    m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
   }
-  */
 
   private void Update() {
-    // Track the current state of the fire button and make decisions based on the current launch force.
+    m_AimSlider.value = m_MinLaunchForce;
+
+    if (Input.GetButtonDown(m_FireButton)) {
+      m_Fired = false;
+      m_CurrentLaunchForce = m_MinLaunchForce;
+      m_ShootingAudio.clip = m_ChargingClip;
+      m_ShootingAudio.Play();
+    } else if (Input.GetButton(m_FireButton) && !m_Fired) {
+      m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+      m_AimSlider.value = m_CurrentLaunchForce;
+    } else if (Input.GetButtonUp(m_FireButton) && !m_Fired) {
+      Fire();
+    } else if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
+      m_CurrentLaunchForce = m_MaxLaunchForce;
+      Fire();
+    }
   }
 
-
   private void Fire() {
-    // Instantiate and launch the shell.
+    m_Fired = true;
+    Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position,
+      m_FireTransform.rotation) as Rigidbody;
+    shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+    m_ShootingAudio.clip = m_FireClip;
+    m_ShootingAudio.Play();
+    m_CurrentLaunchForce = m_MinLaunchForce;
   }
 }
