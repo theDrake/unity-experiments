0eacdd536728152ca54aa03a1cde2907e4190ab0
David C. Drake <theDrake@users.noreply.github.com>
2025-10-22 22:55:00 -0600

    Update 'Rollplay' for Unity 6.2

--- Rollplay/Assets/Scripts/TrackFinish.cs
+++ Rollplay/Assets/Scripts/TrackFinish.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class TrackFinish : MonoBehaviour {
-  private Vector3 _positionModifier = new(0, 0, 0.01f);
+  private Vector3 _positionModifier = new(0, 0, 0.005f);
 
   private void Update() {
     transform.position += _positionModifier;
